86e6e2f9a1cbbfd09f807754a9153fce22c25210
Mario Bešen (Span) <mbesen@span.eu>
2019-01-21 11:14:43 +0100

    It still doesn't work, but I'm close

--- SpotlightExtractor.ps1
+++ SpotlightExtractor.ps1
@@ -7,7 +7,7 @@ set-location $env:USERPROFILE\AppData\Local\Packages\Microsoft.Windows.ContentDe
 
 Write-Host "Where would you like to save pictures?"
 
-New-Item -ItemType Directory -Path C:\ -Name Spotlight
+New-Item -ItemType Directory -Path C:\ -Name Spotlight -Force
 Copy-Item $env:USERPROFILE\AppData\Local\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets -Destination c:\Spotlight -Recurse
 Set-Location C:\Spotlight
 Get-ChildItem | %{Rename-Item $_ -NewName ("SpotlightPhoto_{0}.jpg"-f $nr++)}
