7e5f8f5a79db92035d7958e04062db452ae03981
Mario Bešen (Span) <mbesen@span.eu>
2019-01-21 09:04:00 +0100

    Update (should work)

--- SpotlightExtractor.ps1
+++ SpotlightExtractor.ps1
@@ -3,9 +3,11 @@ Clear-Host
 #Spotlight Picture Extractor
 # 2019 Mario Besen
 
-$SpotlightFolder = set-location $env:USERPROFILE\AppData\Local\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets
+set-location $env:USERPROFILE\AppData\Local\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets
 
 Write-Host "Where would you like to save pictures?"
 
 New-Item -ItemType Directory -Path C:\ -Name Spotlight
-Copy-Item $SpotlightFolder -Destination c:\Spotlight -force
+Copy-Item $env:USERPROFILE\AppData\Local\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets -Destination c:\Spotlight -Recurse
+Set-Location C:\Spotlight
+Get-ChildItem | %{Rename-Item $_ -NewName ("SpotlightPhoto_{0}.jpg"-f $nr++)}
